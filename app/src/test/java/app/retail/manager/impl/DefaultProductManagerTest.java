51a0e0cc523953ab3c578bf0fb3d3d7293d8ff15
Christian <christian-x7h@outlook.com>
2022-06-14 12:44:22 -0600

    Add test scaffolding

---
+++ app/src/test/java/app/retail/manager/impl/DefaultProductManagerTest.java
@@ -0,0 +1,11 @@
+package app.retail.manager.impl;
+
+import org.junit.jupiter.api.Test;
+
+public class DefaultProductManagerTest {
+
+    @Test
+    public void testGetProduct() {
+        // TODO implement a unit test using mocks
+    }
+}
---
+++ app/src/test/java/integration/ComponentIntegrationTest.java
@@ -0,0 +1,6 @@
+package integration;
+
+// TODO setup component integration test: test-specific configuration, mocks, etc.
+//   then test the product endpoint calls/responses
+public class ComponentIntegrationTest {
+}
